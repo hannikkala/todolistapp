4b931e7bfa51c28a6f2b6d5dc575b5a6206ca900
Tommi Hännikkälä <tommi.hannikkala@siili.fi>
2015-12-08 10:06:11 +0200

    Initial commit.

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('unitytodoApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
